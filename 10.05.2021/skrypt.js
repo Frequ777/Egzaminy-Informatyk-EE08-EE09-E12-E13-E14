d35d0d6b42b316a9c4aaf3cfc5dc8fac7616446e
Michał Jan Lewandowski <91124839+Frequ777@users.noreply.github.com>
2021-12-07 21:02:18 +0100

    Add files via upload

---
+++ 10.05.2021/skrypt.js
@@ -0,0 +1,12 @@
+document.write("Hello world :)!");
+
+alert("Witaj człowieku!");
+confirm("Oj confirm taktak");
+var a = 23;
+var b = 56;
+var c = "Michał";
+document.write("<p>"+ a + b + c +"</p>");
+document.write("<h1>"+a+"</h1>")
+document.write("<p class='zielony'>"+b+"</p>")
+document.write("<b>"+c+"</b>")
+document.write(echo a+b)
---
+++ 10.05.2021/skrypt2.js
@@ -0,0 +1,18 @@
+var a = 5;
+var b = 9;
+document.write(a+b)
+document.write("<br>")
+document.write("Suma A oraz B wynosi: ",a+b,"<br>")
+document.write("Różnica A oraz B wynosi: ",a-b,"<br>")
+document.write("iloraz A oraz B wynosi: ",a*b,"<br>")
+document.write("iloczyn A oraz B wynosi: ",a/b,"<br>")
+document.write("róznica z dzielenia A oraz B wynosi: ",a%b,"<br>")
+
+var imie = prompt("Podaj Imie", "np. Marek");
+var nazwisko = prompt("Podaj Nazwisko", "np. Kowalski");
+var dataur = prompt("Podaj Datę Urodzenia", "np. 20 Grudnia");
+var zamie = prompt("Podaj Adres Zamieszkania", "np. Ciechanów");
+document.write("Imię: " , "<br>" +imie, "<br>");
+document.write("Nazwisko: " , "<br>" +nazwisko, "<br>");
+document.write("Data Urodzenia: " , "<br>" +dataur, "<br>");
+document.write("Adres Zamieszkania: " , "<br>" +zamie, "<br>");
